fce9a5ad42ce5121e0a428048aa89f0d4a5aabb8
Arun200404 <arun173153@email.com>
2025-06-27 17:56:29 +0530

    Add Firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAj2TvxKMEfej38v1nTr6bdTI8Ox4w_urA",
+  authDomain: "notes-app-68eb9.firebaseapp.com",
+  projectId: "notes-app-68eb9",
+  storageBucket: "notes-app-68eb9.firebasestorage.app",
+  messagingSenderId: "1054623068437",
+  appId: "1:1054623068437:web:36b64d3ef112296727791c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
